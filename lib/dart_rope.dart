0de435298cb3adad25e521f2ffc274604e0e59a4
Peter-Van-Drunen <peetevd@gmail.com>
2017-10-30 15:16:18 -0400

    Implemented .split() operation

--- lib/dart_rope.dart
+++ lib/dart_rope.dart
@@ -1,28 +1,63 @@
 class Rope {
   //Fields
-  _Node _root;
-  num length;
+  _Rope_Node _root;
+  num _length;
 
   //Constructors
 
   //Default Constructor
   Rope()
-      : _root = new _Node(),
-        length = 0;
+      : _root = new _Rope_Node(),
+        _length = 0;
 
   //String Constructor
   Rope.init(String input)
-      : _root = new _Node.init(input),
-        length = input.length;
+      : _root = new _Rope_Node.init(input),
+        _length = input.length;
 
   //Shallow Copy constructor
-  Rope._from(_Node new_root) : _root = new_root;
+  //Invoking this constuctor will yield a rope with the wrong length. You much set this manually in the calling method.
+  //TODO: Fix above issue. Likely a pretty big refactor.
+  Rope._from(_Rope_Node new_root)
+      : _root = new_root,
+        _length = 0;
+
+  //Split method that splits the rope in place, returning the 2nd half.
+  Rope split(int index) {
+    if (this._root.isShared()) {
+      this._root = _Rope_Node.move(this._root);
+    }
+    if (index > this._length - 1 || index < 0) {
+      throw new Exception(
+          "Invalid argument in Rope:split(). Index out of bounds.");
+    }
+    if (index == 0) {
+      Rope temp = new Rope._from(this._root);
+      temp._length = this._length;
+      this._root = null;
+      this._length = 0;
+      return temp;
+    }
+
+    List<_Rope_Node> orphans = this._root.split(index);
+
+    Rope new_rope = new Rope._from(orphans.removeAt(0));
+
+    while (orphans.length > 0) {
+      new_rope = new_rope + new Rope._from(orphans.removeAt(0));
+    }
+
+    new_rope._length = this._length - index;
+    this._length = index;
+
+    return new_rope;
+  }
 
   //Operator overloads
 
   //Index getter
   String operator [](int index) {
-    if (index > this.length - 1 || index < 0) {
+    if (index > this._length - 1 || index < 0) {
       throw new Exception("Invalid argument in Rope:[]. Index out of bounds.");
     }
     return _root.getIndex(index);
@@ -33,64 +68,117 @@ class Rope {
     if (val.length != 1) {
       throw new Exception(
           "Invalid argument in Rope:[]=. Input value must be of length 1.");
-    } else if (index > this.length - 1 || index < 0) {
+    } else if (index > this._length - 1 || index < 0) {
       throw new Exception("Invalid argument in Rope:[]=. Index out of bounds.");
     }
     if (_root.isShared()) {
-      _root = _Node.move(_root);
+      _root = _Rope_Node.move(_root);
     }
     _root.setIndex(index, val);
   }
 
   Rope operator +(Rope other) {
-    _Node new_root = new _Node();
+    _Rope_Node new_root = new _Rope_Node();
     new_root.l = this._root;
     new_root.r = other._root;
 
-    new_root.weight = new_root.l.getSize();
+    new_root.weight = new_root.l.getWeight();
 
     Rope new_rope = new Rope._from(new_root);
-    new_rope.length = this.length + other.length;
+    new_rope._length = this._length + other._length;
 
     this._root.addRefs();
     other._root.addRefs();
 
     return new_rope;
   }
+
+  //length getter
+  num getLength() {
+    return _length;
+  }
 }
 
-class _Node {
+class _Rope_Node {
   //Fields
-  _Node l, r;
+  _Rope_Node l, r;
   num weight;
   String val;
   num refs;
 
   //Constructors
-  _Node() {
+  _Rope_Node() {
     this.weight = 0;
     this.refs = 1;
   }
 
-  _Node.init(String input) {
+  _Rope_Node.init(String input) {
     this.refs = 1;
     if (input.length <= 10) {
       this.val = input;
       this.weight = input.length;
     } else {
-      this.l = new _Node.init(input.substring(0, input.length ~/ 2));
-      this.r = new _Node.init(input.substring(input.length ~/ 2, input.length));
+      this.l = new _Rope_Node.init(input.substring(0, input.length ~/ 2));
+      this.r =
+      new _Rope_Node.init(input.substring(input.length ~/ 2, input.length));
 
-      this.weight = this.l.getSize();
+      this.weight = this.l.getWeight();
     }
   }
 
-  //Static method for moving the reference to a node if we are mutating it
-  static _Node move(_Node it) {
+  //Static method for moving the reference to a node if we are mutating it and it has multiple references
+  static _Rope_Node move(_Rope_Node it) {
     String content = it.getContent();
-    _Node new_node = new _Node.init(content);
+    _Rope_Node new_node = new _Rope_Node.init(content);
     it.removeRefs();
     return new_node;
+  }
+
+  //Method for splitting a rope at a given index. Returns orphaned nodes which are re-concated in the parent function
+  List<_Rope_Node> split(int index) {
+    if (this.weight <= index) {
+      //When we travel to the left on our way up the tree, we will never orphan a node, so simply carry whatever values are present onto the next node.
+      List<_Rope_Node> orphans = this.r.split(index - this.weight);
+
+      if (orphans.length == 1 && this.r.isLeaf()) {
+        this.r = null;
+      }
+
+      return orphans;
+    } else {
+      if (this.l != null) {
+        List<_Rope_Node> orphans = this.l.split(index);
+
+        //If the left child node contains the index, orphan that node and add it to the list
+        if (orphans.length > 0) {
+          //add the right node to the list of orphans
+          orphans.add(this.r);
+          this.r = null;
+
+          //re-define the weight of this node taking into account any changes that may have occured further down the tree
+          this.weight = this.l.getWeight();
+        }
+        return orphans;
+      } else {
+        if (index == 0) {
+          //Return a list with this as it's only element
+          List<_Rope_Node> new_orphan_list = new List<_Rope_Node>();
+          new_orphan_list.add(this);
+          return new_orphan_list;
+        } else {
+          //create a left node, return an orphaned right node
+          this.l = new _Rope_Node.init(this.val.substring(0, index));
+          _Rope_Node orphan =
+          new _Rope_Node.init(this.val.substring(index, this.val.length));
+          this.val = null;
+          this.weight = this.l.weight;
+
+          List<_Rope_Node> new_orphans_list = new List<_Rope_Node>();
+          new_orphans_list.add(orphan);
+          return new_orphans_list;
+        }
+      }
+    }
   }
 
   //Getter for indexing operation
@@ -110,13 +198,13 @@ class _Node {
   void setIndex(int index, String val) {
     if (this.weight <= index) {
       if (this.r.isShared()) {
-        this.r = _Node.move(this.r);
+        this.r = _Rope_Node.move(this.r);
       }
       this.r.setIndex(index - this.weight, val);
     } else {
       if (this.l != null) {
         if (this.l.isShared()) {
-          this.l = _Node.move(this.l);
+          this.l = _Rope_Node.move(this.l);
         }
         this.l.setIndex(index, val);
       } else {
@@ -154,11 +242,11 @@ class _Node {
   }
 
   //Utility method that gets the weight of the node
-  num getSize() {
+  num getWeight() {
     if (this.isLeaf()) {
       return this.weight;
     } else {
-      return this.l.getSize() + (this.r != null ? this.r.getSize() : 0);
+      return this.l.getWeight() + (this.r != null ? this.r.getWeight() : 0);
     }
   }
 
@@ -181,3 +269,4 @@ class _Node {
     return this.refs > 1;
   }
 }
+
--- test/rope_test.dart
+++ test/rope_test.dart
@@ -5,14 +5,14 @@ void main() {
   test('default constructor', () {
     Rope r1 = new Rope();
 
-    expect(r1.length, 0);
+    expect(r1.getLength(), 0);
   });
 
   test('init constructor', () {
     Rope r1 = new Rope.init(
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.");
 
-    expect(r1.length, 123);
+    expect(r1.getLength(), 123);
   });
 
   test('index access', () {
@@ -31,22 +31,63 @@ void main() {
     expect(r1[24], "G");
   });
 
-  test('concat', () {
+  test('concat operator +', () {
     Rope r1 = new Rope.init(
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ");
     Rope r2 = new Rope.init(
         "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.");
 
     Rope r3 = r1 + r2;
-    expect(r3[r1.length], "U");
-    expect(r3[r3.length - 1], ".");
+    expect(r3[r1.getLength()], "U");
+    expect(r3[r3.getLength() - 1], ".");
 
     r3[0] = "X";
     expect(r1[0], "L");
     expect(r3[0], "X");
 
     r2[0] = "X";
-    expect(r3[r1.length], "U");
+    expect(r3[r1.getLength()], "U");
     expect(r2[0], "X");
+
+    expect(r3.getLength(), r1.getLength() + r2.getLength());
+  });
+
+  test('split', () {
+    //split on the "end" of a node
+    Rope r1 = new Rope.init(
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ");
+    Rope r2 = r1.split(23);
+
+    expect(r1.getLength(), 23);
+    expect(r1[22], "a");
+    expect(r2[0], "m");
+
+    //split in the "middle" of a node
+    Rope r3 = new Rope.init(
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ");
+    Rope r4 = r3.split(22);
+
+    expect(r3.getLength(), 22);
+    expect(r3[21], " ");
+    expect(r4[0], "a");
+
+    //edge case of splitting on the 0 index
+    Rope r5 = new Rope.init(
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ");
+    Rope r6 = r5.split(0);
+
+    expect(r5.getLength(), 0);
+    expect(r6.getLength(), 124);
+    expect(r6[0], "L");
+    expect(r6[123], " ");
+
+    //edge case of splitting on last index
+    Rope r7 = new Rope.init(
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ");
+    Rope r8 = r7.split(123);
+
+    expect(r8[0], " ");
+    expect(r7[122], ".");
+    expect(r8.getLength(), 1);
   });
 }
